dbe691792548be78ebb8cc9f16e9e9daac5f9543
mrshukla1805 <nageshshukla1805@gmail.com>
2023-06-05 00:39:22 +0530

    removed creator field from simulation

--- x/user/simulation/create_user.go
+++ x/user/simulation/create_user.go
@@ -3,12 +3,31 @@ package simulation
 import (
 	"math/rand"
 
+	"user/x/user/keeper"
+	"user/x/user/types"
+
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-	"user/x/user/keeper"
-	"user/x/user/types"
 )
+
+// func SimulateMsgCreateUser(
+// 	ak types.AccountKeeper,
+// 	bk types.BankKeeper,
+// 	k keeper.Keeper,
+// ) simtypes.Operation {
+// 	return func(r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
+// 	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
+// 		simAccount, _ := simtypes.RandomAcc(r, accs)
+// 		msg := &types.MsgCreateUser{
+// 			Creator: simAccount.Address.String(),
+// 		}
+
+// 		// TODO: Handling the CreateUser simulation
+
+// 		return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "CreateUser simulation not implemented"), nil, nil
+// 	}
+// }
 
 func SimulateMsgCreateUser(
 	ak types.AccountKeeper,
@@ -17,9 +36,10 @@ func SimulateMsgCreateUser(
 ) simtypes.Operation {
 	return func(r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
 	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
-		simAccount, _ := simtypes.RandomAcc(r, accs)
+		_, _ = simtypes.RandomAcc(r, accs)
 		msg := &types.MsgCreateUser{
-			Creator: simAccount.Address.String(),
+			Name:     "test",     // Provide a sample name value for testing
+			Password: "password", // Provide a sample password value for testing
 		}
 
 		// TODO: Handling the CreateUser simulation
